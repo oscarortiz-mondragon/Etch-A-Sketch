6bb2be7b5bca80774cab18243b9b68a9e7036dd4
Oscar Ortiz-Mondragon <oscarortiz@utexas.edu>
2020-07-25 00:09:03 -0500

    created the grid, now I need to work on styling them with DOM

--- script.js
+++ script.js
@@ -1,63 +1,27 @@
-
-let button = document.querySelector("input")
 let container = document.querySelector("#container")
-let startGrid = document.createElement('div')
-let cell = document.createElement("div");
-let atb =  cell.getAttribute('style')  
+let button = document.querySelector("input")
+// 16 by 16 start off
+container.style.cssText = ("display: grid; --grid-rows: 16; --grid-cols: 16; row-gap: 1px; column-gap: 1px; width: 569.6px; height: 569.6px; margin: auto;")
+for(let i = 0; i < (16 * 16); i++){
+  let div = document.createElement('div')
+  div.setAttribute('style',"background-color:black;")
+  container.appendChild(div).id = "original"
+}  
 
-// // Start with 16 by 16
-let onPageGrid = function(){
-startGrid.setAttribute("style", "height:20px; width:20px; background:red;")
-container.appendChild(startGrid)}
+document.getElementById( 
+  "original").addEventListener( 
+  "mouseover", over); 
 
+document.getElementById( 
+  "original").addEventListener( 
+  "mouseout", out); 
 
-function makeRows(rows, cols) {
+  function over() { 
+    document.getElementById( 
+      "original").style["background-color"] = "green"; 
+  } 
+  function out() { 
+    document.getElementById( 
+      "original").style["background-color"] = "white"; 
+  } 
   
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (i = 0; i < (rows * cols); i++) {
-    let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
-  };
-};
-
-makeRows(16, 16);
-
-
-// Creates a Grid with your choice of size. 
-button.onclick = 
-
-function createGrid () {
-    let gridSize = prompt("How large do you want it?");
-  
-if(gridSize <= 0){
-    alert("please choose a number greater than 0")
-   createGrid()
-}
-else if (gridSize > 0) {
-
-  container.style.setProperty('--grid-rows', gridSize);
-    container.style.setProperty('--grid-cols', gridSize);
-    for (i = 0; i < (gridSize * gridSize); i++) {
-      let cell = document.createElement("div");
-      container.appendChild(cell).className = "your-grid";
-    
-    };
-    let test = document.getElementsByClassName("grid-item");
-    test.classList.remove("grid-item");
-}
-else{
-    alert("please choose a number");
-    createGrid();
-}
-
-
-}
-document.getElementById("gridContainer").style.cssText = (`display: grid;
-    grid-template-columns: repeat(` + grid + `,`+ cellHeight + `px);
-    grid-template-rows: repeat(` + grid + `,` + cellHeight + `px);
-    grid-gap: ` + padding + `px; width: ` + gridContainerHeight + `px;
-    height: ` + gridContainerHeight + `px;`);
-//   Need to create a way to delete the previous entry
-
-// make it part of the style, that way you don't have to add remove divs.
