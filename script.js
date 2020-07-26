f705c7720c57fa498e2f9adc1f88922797ab956f
Oscar Ortiz-Mondragon <oscarortiz@utexas.edu>
2020-07-26 01:50:22 -0500

    made the grid, need to fix the issue with erasing previous grids

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 let container = document.querySelector("#container")
-let button = document.querySelector("input")
-// 16 by 16 start off
-container.style.cssText = ("display: grid; --grid-rows: 16; --grid-cols: 16; row-gap: 1px; column-gap: 1px; width: 569.6px; height: 569.6px; margin: auto;")
-for(let i = 0; i < (16 * 16); i++){
-  let div = document.createElement('div')
-  div.setAttribute('style',"background-color:black;")
-  container.appendChild(div).id = "original"
-}  
 
-document.getElementById( 
-  "original").addEventListener( 
-  "mouseover", over); 
+container.style.cssText = "display:grid; margin: auto;height: 560px; width:500px;grid-gap:10px; grid-template-rows: repeat(16,1fr); grid-template-columns:repeat(16,1fr);"
+  for(let i = 0; i < 256; i++)
+      {
+        let div = document.createElement('div')
+        div.style.cssText = "background-color:red"
+        div.id = "default-grid" 
+        container.appendChild(div)
+      }
+      
+      
+let button = document.getElementById("Shake")
 
-document.getElementById( 
-  "original").addEventListener( 
-  "mouseout", out); 
+ button.onclick =function ()
+  {
+    let yourChoice = prompt("how large do you want it?")
+      if(yourChoice > 0)
+           {
+            container.style.cssText = "display:grid; margin: auto;height: 560px; width:500px;grid-gap:10px; grid-template-rows: repeat(" +yourChoice+ ",1fr); grid-template-columns:repeat(" +yourChoice+",1fr);" 
+            for(let i = 0; i < (yourChoice * yourChoice); i++)
+              {
+                let div = document.createElement('div')
+                div.id = "default-grid"
+                div.style.cssText = "background-color:green;"
+                container.appendChild(div)
+              }
+          }
+      else 
+          {
+            alert("please choose a number geater than 0")
+          return button.onclick()
+          }
+          for(let i = 0; i < 256; i++)
+          {
+            let div = document.getElementById("default-grid")
+            container.removeChild(div)
+          }
+  }
 
-  function over() { 
-    document.getElementById( 
-      "original").style["background-color"] = "green"; 
-  } 
-  function out() { 
-    document.getElementById( 
-      "original").style["background-color"] = "white"; 
-  } 
-  
